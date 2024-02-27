c13dfaa80178c8c6397fa8d4e43701e0164eeb46
hoffmanick <134728053+hoffmanick@users.noreply.github.com>
2024-02-27 18:27:20 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   .devcontainer/devcontainer.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   server.js
    	modified:   yarn.lock

--- server.js
+++ server.js
@@ -13,6 +13,24 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: "testid.c9s268qmu83p.us-west-1.rds.amazonaws.com",
+  user: "admin",
+  password: "data_for_7?",
+  database: 'insite_specs_test'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM specimens", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
 app.get('/', (req, res) => {
 	res.send('Hello remote world!\n');
 });
